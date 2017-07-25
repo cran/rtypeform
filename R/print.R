99346d990b11a113f6efa066b8c23a861beab4ee
Colin Gillespie <csgillespie@gmail.com>
2017-07-25 20:49:25 +0000

    version 0.3.1

--- R/print.R
+++ R/print.R
@@ -1,11 +1,18 @@
 #' @export
-print.rtypeform_results = function(x, ...) {
-  cat("http_status:", x$http_status)
-  cat("\nQuestion statistics:\n")
-  cat("\t Total:", x$stats$total)
-  cat("\n\t Completed:", x$stats$completed)
-  cat("\n\t Uncompleted:", x$stats$uncompleted)
-  cat("\n\t Response rate:", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+print.rtypeform_all = function(x, ...) {
+  print(x$content)
+  cat("\nhttp_status:", x$http_status, "OK")
+  cat("\n", nrow(x$content), "questionnaires")
   invisible(x)
 }
 
+#' @export
+print.rtypeform_results = function(x, ...) {
+  cat("http_status:", x$http_status, "OK")
+  cat("\nQuestion statistics:\n")
+  cat("\t Total:", x$stats$total)
+  cat("\n\t Completed:", x$stats$completed)
+  cat("\n\t Uncompleted:", x$stats$total - x$stats$completed)
+  cat("\n\t Response rate: ", signif(x$stats$completed/x$stats$total, 3)*100, "%", sep="")
+  invisible(x)
+}
